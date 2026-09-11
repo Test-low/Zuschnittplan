ca181c4bb3ce5cda206e1af9724db69a65a31b43
Test-low <schneiderst94@gmail.com>
2026-09-11 11:33:40 +0200

    V42

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="lowis-zuschnittplaner-v41";
+const CACHE="lowis-zuschnittplaner-v42";
 const STATIC_ASSETS=["./manifest.webmanifest","./icon.svg"];
 
 self.addEventListener("install",event=>{
